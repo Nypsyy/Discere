11ac3e4c37a08a728f171ccbb9a39e7118872059
JulienVernay <contact@jvernay.fr>
2021-02-01 10:22:08 +0100

    Reuse of Rewired prefab for ProtoArena + Reorganization of Hero

--- Discere/Assets/Scripts/Hero.cs
+++ Discere/Assets/Scripts/Hero.cs
@@ -9,10 +9,17 @@ public class Hero : MonoBehaviour {
 
     private Player player;
     private Rigidbody2D body;
+    private Vector2 input_vec;
 
     void Awake() {
-    	player = ReInput.players.GetPlayer(0);
-    	body = GetComponent<Rigidbody2D>();
+        player = ReInput.players.GetPlayer(0);
+        body = GetComponent<Rigidbody2D>();
+        input_vec = new Vector2(0,0);
+        
+        // Controller map in gameplay mode
+        player.controllers.maps.mapEnabler.ruleSets.Find(rs => rs.tag == "Gameplay").enabled = true;
+        player.controllers.maps.mapEnabler.ruleSets.Find(rs => rs.tag == "UI").enabled = false;
+        player.controllers.maps.mapEnabler.Apply();
     }
 
     // Start is called before the first frame update
@@ -22,8 +29,11 @@ public class Hero : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        Vector2 vec = new Vector2(player.GetAxis("MoveHorizontal"),
-                                  player.GetAxis("MoveVertical"));
-        body.velocity = vec * speed;
+        input_vec = player.GetAxis2D("Move Horizontal", "Move Vertical");
+        body.velocity = input_vec * speed;
+    }
+    
+    void FixedUpdate() {
+        body.velocity = input_vec * speed;
     }
 }
