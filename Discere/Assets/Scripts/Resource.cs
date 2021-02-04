5e62fa6dc3928508eb218450f8aaa95dde46b0d7
arnette-thomas <thomas.arnette@outlook.fr>
2021-02-04 15:46:22 +0100

    Added transition effect on damage taken

--- Discere/Assets/Scripts/Resource.cs
+++ Discere/Assets/Scripts/Resource.cs
@@ -14,6 +14,12 @@ public class Resource : MonoBehaviour
     public float minValue = 0f, maxValue = 100f;    // Bounds
     public UnityEvent emptyEvt, fullEvt;            // Events
 
+    [Header("Transition")]
+    private Slider transitionSlider = null;
+    public float transitionWaitingTime = 1f;
+    public float transitionTime = 0.2f;
+    private short transitionCnt = 0;
+
     public float value { get; private set; }
     
     // Start is called before the first frame update
@@ -29,14 +35,57 @@ public class Resource : MonoBehaviour
         ui.maxValue = maxValue;
         ui.minValue = minValue;
         ui.value = maxValue;
+
+        // Check if ui has a slider for transition
+        Slider[] sliders = ui.GetComponentsInChildren<Slider>();
+        if (sliders.Length > 1)
+        {
+            transitionSlider = sliders[1];
+            transitionSlider.maxValue = maxValue;
+            transitionSlider.minValue = minValue;
+            transitionSlider.value = maxValue;
+        }
     }
 
-    public void ChangeValue(float amount)
+    protected void ChangeValue(float amount)
     {
         value = Mathf.Clamp(value + amount, minValue, maxValue);
-        if (ui != null) ui.value = value;
+
+        // Manage visual
+        if (ui != null)
+        { 
+            ui.value = value; 
+            if (transitionSlider != null)
+            {
+                StartCoroutine(ChangeTransitionValue());
+            }
+        }
 
         if (value <= minValue) emptyEvt.Invoke();
         if (value >= maxValue) fullEvt.Invoke();
+    }
+
+    // Coroutine changing value of transitionSlider
+    private IEnumerator ChangeTransitionValue()
+    {
+        // Initial wait
+        transitionCnt++;
+        yield return new WaitForSeconds(transitionWaitingTime);
+        transitionCnt--;
+        if (transitionCnt > 0) yield break;
+
+        // Shrinking slider
+        float transitionTimer = 0f;
+        float startValue = transitionSlider.value;
+        while (transitionTimer <= transitionTime)
+        {
+            transitionSlider.value = Mathf.Lerp(startValue, ui.value, transitionTimer / transitionTime);
+            yield return null;
+            if (transitionCnt > 0) yield break;
+            transitionTimer += Time.deltaTime;
+        }
+
+        // Making sure both slider are the same value in the end
+        transitionSlider.value = ui.value;
     }
 }
