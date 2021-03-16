710d66b11b2293a35814861fdfb440561d88532e
arnette-thomas <thomas.arnette@outlook.fr>
2021-03-16 16:21:34 +0100

    Added options to change volume

--- Discere/Assets/Scripts/HUD/Options.cs
+++ Discere/Assets/Scripts/HUD/Options.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class Options : MonoBehaviour
 {
+    private new AudioManager audio;
+    private MusicManager music;
+
     private void Start()
     {
         Screen.fullScreenMode = FullScreenMode.FullScreenWindow;
+        audio = FindObjectOfType<AudioManager>();
+        music = audio.GetComponentInChildren<MusicManager>();
     }
 
     public void ToggleFullScreen(bool fullScreen)
     {
         Screen.fullScreen = fullScreen;
+    }
+
+    public void ChangeMusicVolume(float value)
+    {
+        music.musicSource.volume = value;
+    }
+
+    public void ChangeSFXVolume(float value)
+    {
+        audio.globalVolumeMultiplier = value;
     }
 }
